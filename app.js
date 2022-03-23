b0a012ad0ebd3dc7b1c87f2c260891cf038f6a14
Vladimir Salguero <vladimir891@hotmail.com>
2022-03-23 17:06:34 -0600

    feat: initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+//package dotenv for use environment variables from .env file
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+//avoid compatibility problems with browsers
+app.use(cors);
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`App listen http://localhost:${port}`);
+});
---
+++ config/mongo.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const dbConnect = () => {
+
+
+}
+
+module.exports = dbConnect;

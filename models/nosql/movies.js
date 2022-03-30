d85113537cbccd1e42250cf3675817a2de7ace6e
Vladimir Salguero <vladimir891@hotmail.com>
2022-03-30 11:59:26 -0600

    feat: adding movies and storages model MongoDB

---
+++ models/nosql/movies.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const MovieScheme = new mongoose.Schema(
+    {
+        name: {
+            type:String
+        },
+        summary: {
+            type: String
+        },
+        genre: {
+            type: String
+        },
+        poster: {
+            type: String,
+            validate: {
+                validator: (req) => {
+                    return true;
+                },
+                message: "ERROR_URL",
+            },
+        },
+        director: {
+            type: String,
+        },
+        date_release: {
+            type: Date,
+        },
+        mediaId: {
+            type: mongoose.Types.ObjectId,
+        }
+    },
+    {
+        timestamps: true, //TODO: createdAt, updatedAt
+        versionKey: false
+    }
+);
+
+module.exports = mongoose.model("movies", MovieScheme);
---
+++ models/nosql/storages.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const StorageScheme = new mongoose.Schema(
+    {
+        url: {
+            type:String
+        },
+        filename: {
+            type: String
+        }
+    },
+    {
+        timestamps: true,
+        versionKey: false
+    }
+);
+
+module.exports = mongoose.model("storages", StorageScheme);
